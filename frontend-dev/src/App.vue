35e54a92f1ada18928f363414d642c18a0c233d0
Lewis-lv0 <1806048207@qq.com>
2022-03-01 00:54:44 +0000

    user login

--- frontend-dev/src/App.vue
+++ frontend-dev/src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+
   <router-view/>
 </template>
 
@@ -11,7 +8,6 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
---
+++ frontend-dev/src/components/Login.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container-fluid">
+
+        <div class="row align-items-center">
+            <div class="col-6 offset-3">
+                <form>
+                    <!-- website logo -->
+                    <div class="avatar-box">
+                        <img src="../assets/logo.png" alt="" >
+                    </div>
+
+
+                    <div class="mb-3 align-self-start">
+                        <label for="userInputEmail1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="userInputEmail1">
+                    </div>
+
+                    <div class="mb-3 align-self-start">
+                        <label for="userInputPassword" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="userInputPassword">
+                    </div>
+                    <div class="login-button-box">
+                        <button type="submit" class="btn btn-primary">Log in</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style lang="scss" scoped>
+.avatar-box {
+    text-align: center;
+}
+.login-button-box {
+    text-align: center;
+}
+</style>
