2e259e34ecdc05764635221291cddee16160c6f2
Lewis-lv0 <1806048207@qq.com>
2022-03-03 13:47:39 +0000

    update

--- frontend-dev/src/components/Register.vue
+++ frontend-dev/src/components/Register.vue
@@ -7,7 +7,7 @@
                 <div class="avatar-box">
                     <img src="../assets/logo.png" alt="" >
                 </div>
-                <form :model="registerForm" >
+                <form>
                     <div class="mb-3 align-self-start">
                         <label for="Username" class="form-label">Username</label>
                         <input v-model="registerForm.userName" type="text" class="form-control" id="Username"  required>
@@ -22,8 +22,9 @@
                     </div>
                     <div class="mb-3 align-self-start">
                         <label for="userConfirmPassword" class="form-label">Confirm password</label>
-                        <input v-model="registerForm.confirmPassword" type="password" class="form-control" id="userConfirmPassword" required>
-                        <p id="password-not-confirm" style="display:none; color: red" v-show="registerForm.password!=registerForm.confirmPassword">Your password is not consistent</p>
+                        <input v-model="registerForm.confirmPassword" @blur="pwdNotSame()"
+                        type="password" class="form-control" id="userConfirmPassword" required>
+                        <p id="password-not-confirm" style="visibility:hidden; color: red">Your password is not consistent</p>
                     </div>
                     <div class="login-button-box">
                         <button type="submit" class="btn btn-primary" @click="register">Register</button>
@@ -36,10 +37,12 @@
 </template>
 
 <script>
+    import axios from 'axios';
+
     export default {
         name: 'Register',
         data(){
-            return{
+            return {
                 registerForm: {
                     userName: '',
                     email: '',
@@ -49,16 +52,25 @@
             }
         },
         methods:{
-            register(){
+            register() {
+                console.log("register is clicked")
                 const result=axios.request({
                     url:'',
                     method:'post',
                     headers:{
                         'Content-type':'text/plain'
                     },
-                    data:this.registerForm
+                    data: this.registerForm
                 });
                 console.log(result)
+            },
+
+            pwdNotSame() {
+                if (this.registerForm.password != this.registerForm.confirmPassword) {
+                    console.log("Pwd not the same")
+                    var feedback = document.getElementById("password-not-confirm")
+                    feedback.style.visibility = 'visible'
+                }
             }
         }
     }
