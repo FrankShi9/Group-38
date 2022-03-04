1a3d420e8a5f6c54c8cde6984131e6069cb2578e
Lewis-lv0 <1806048207@qq.com>
2022-03-04 22:57:03 +0000

    update

--- Area_38_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Area38AppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Area_38_app'
--- Area_38_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-
-
-class UserInfo(models.Model):
-    username = models.CharField(max_length=32)
-    password = models.CharField(max_length=64)
