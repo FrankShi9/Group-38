0a2fd192bfc003fac274d1dc15aa504b18553f1a
Pony-Lamb <93828053+Pony-Lamb@users.noreply.github.com>
2022-04-11 05:43:27 +0800

    Update urls.py

--- Area_38/Area_38_app/urls.py
+++ Area_38/Area_38_app/urls.py
@@ -4,15 +4,15 @@ from . import views
 
 
 urlpatterns = [
-    path('login/', views.login),
-    path('register/', views.register),
-    path('forget/', views.forget),
-    path('SDChart/', views.SDChart),
-    path('RFMChart/', views.RFMChart),
-    path('TSChart/', views.TSChart),
-    path('uploadfile/', views.uploadfile),
-    path('chooseFunc/', views.chooseFunc),
-    path('demand/', views.demand),
-    path('rfm/', views.rfm),
-    path('xts/', views.xts),
+    path('login/', views.login,name='login'),
+    path('register/', views.register,name='register'),
+    path('forget/', views.forget,name='forget'),
+    path('SDChart/', views.SDChart,name='SDChart'),
+    path('RFMChart/', views.RFMChart,name='RFMChart'),
+    path('TSChart/', views.TSChart,name='TSChart'),
+    path('uploadfile/', views.uploadfile,name='uploadfile'),
+    path('chooseFunc/', views.chooseFunc,name='chooseFunc'),
+    path('demand/', views.demand,name='demand'),
+    path('rfm/', views.rfm,name='rfm'),
+    path('xts/', views.xts,name='xts'),
 ]
