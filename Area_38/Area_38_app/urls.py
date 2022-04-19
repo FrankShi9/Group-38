d324fd67ba7732e43026334d823166fee0bcf6cd
“Athens0” <zzl445862490@gmail.com>
2022-04-19 23:35:43 +0100

    report download & user history

--- Area_38/Area_38_app/urls.py
+++ Area_38/Area_38_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('login/', views.login),
+    path('login', views.login),
     path('register/', views.register),
     path('forget/', views.forget),
     path('SDChart/', views.SDChart),
@@ -12,14 +12,14 @@ urlpatterns = [
     path('TSChart/', views.TSChart),
     path('uploadfile/', views.uploadfile),
     #path('chooseFunc/', views.chooseFunc),
-    path('home/', views.home),
+    path('home', views.home),
     path('demand/', views.demand),
     path('rfm/', views.rfm),
     path('xts/', views.xts),
     path('logout/',views.logout),
     path('hw/', views.holt_winters),
-    path('AboutUs/', views.AboutUs),
-    path('Guidance/', views.Guidance),
+    path('aboutus', views.AboutUs),
+    path('guidance', views.Guidance),
     path('Admin/', views.Admin),
     path('pdf_download/', views.pdf_download),
     path('history/', views.history),
